15a8295aaecae2936ff5d0b60e64436e960bfdc3
kevin <kevin9160@naver.com>
2022-10-17 14:11:34 +0900

    오류 수정

---
+++ src/main/java/Java_1017/dbexercise/User.java
@@ -0,0 +1,14 @@
+package Java_1017.dbexercise;
+
+public class User {
+
+    private String id;
+    private String name;
+    private String password;
+
+    User(String id, String name, String password){
+        this.id =id;
+        this.name = name;
+        this.password = password;
+    }
+}

9d15994f866dbb3944a2129ff63e57cc6611345d
kevin <kevin9160@naver.com>
2022-10-20 14:49:06 +0900

    다형성 적용을 위한 인터페이스 와 aws connection

--- src/main/java/Java_1020/AWSConn.java
+++ src/main/java/Java_1020/AWSConn.java
@@ -1,2 +1,23 @@
-package Java_1020;public class AWSConn {
+package Java_1020;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Map;
+
+public class AWSConn implements DoConnection{
+    @Override
+    public Connection conectionMaker() throws ClassNotFoundException, SQLException {
+        Map<String, String> env = System.getenv();
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection conn = DriverManager.getConnection(
+                env.get("DB_HOST"), env.get("DB_USER"),env.get("DB_PASSWORD")
+        );
+
+
+
+        return conn;
+    }
 }
--- src/main/java/Java_1020/DoConnection.java
+++ src/main/java/Java_1020/DoConnection.java
@@ -1,2 +1,9 @@
-package Java_1020;public interface DoConnection {
+package Java_1020;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface DoConnection {
+    public Connection conectionMaker() throws ClassNotFoundException, SQLException;
 }
